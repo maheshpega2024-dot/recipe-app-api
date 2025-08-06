a23e60145000d7b93190ba73437b9544aefe7797
Makavg01 <makavg2m01@gmail.com>
2025-08-06 19:26:28 +0530

    user and super user creation

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,58 @@
-from django.db import models #noqa: F401
+""" 
 
-# Create your models here.
+All database models for the application.
+
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+
+    """Manager for custom user model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a user with an email and password."""
+
+        if not email:
+            raise ValueError('User must enter valid email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        user = self.create_user(email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that uses email as the unique identifier."""
+
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
